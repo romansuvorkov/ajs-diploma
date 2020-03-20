0ac91e219e9d229a8b95120514d482a404e4e086
SuvorkovRN <suvorkovRN@gmail.com>
2020-03-20 23:34:43 +0500

    paragraph 8 finished

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -7,6 +7,12 @@ import Undead from './Undead';
 import Magician from './Magician';
 import PositionedCharacter from './PositionedCharacter';
 
+let PlayerTeam = [];
+let ComputerTeam = [];
+let charatersPositions = [];
+const spawnZonePlayer = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
+const spawnZoneComputer = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
@@ -24,25 +30,44 @@ export default class GameController {
     this.computerTeam = generateTeam([Daemon, Vampire, Undead], 1, 2);
     this.computerTeam.teamMembers[0].level = 1;
     this.computerTeam.teamMembers[1].level = 1;
-
-    const spawnZonePlayer = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
-    const spawnZoneComputer = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
-    const spawnedPlayerTeam = generatePosition(spawnZonePlayer, this.playerTeam.teamCount, this.playerTeam.teamMembers, []);
-    const spawnedComputerTeam = generatePosition(spawnZoneComputer,  this.computerTeam.teamCount, this.computerTeam.teamMembers, []);
-    const charatersPositions = [...spawnedPlayerTeam, ...spawnedComputerTeam]
+    PlayerTeam = generatePosition(spawnZonePlayer, this.playerTeam.teamCount, this.playerTeam.teamMembers, []);
+    ComputerTeam = generatePosition(spawnZoneComputer,  this.computerTeam.teamCount, this.computerTeam.teamMembers, []);
+    charatersPositions = [...PlayerTeam, ...ComputerTeam];
     this.gamePlay.redrawPositions(charatersPositions);
 
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
+    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
   }
 
   onCellClick(index) {
     // TODO: react to click
+
+  }
+
+  showCharacterInfo(index, unit) {
+    console.log(`index ${index}`);
+    console.log(`unit ${unit}`);
   }
 
   onCellEnter(index) {
-    // TODO: react to mouse enter
+    let characterInCell = undefined;
+    charatersPositions.forEach(element => {
+      if (element.position === index) {
+            characterInCell = element.character;
+          }
+      return characterInCell;
+    });
+    if (characterInCell !== undefined) {
+      this.gamePlay.showCellTooltip(`${String.fromCodePoint(0x1F396)}:${characterInCell.level}${String.fromCodePoint(0x2694)}:${characterInCell.attack}${String.fromCodePoint(0x1F6E1)}:${characterInCell.defence}${String.fromCodePoint(0x2764)}:${characterInCell.health}`, index)
+    }
+
   }
+
+
 
   onCellLeave(index) {
     // TODO: react to mouse leave
+    this.gamePlay.hideCellTooltip(index);
   }
 }
